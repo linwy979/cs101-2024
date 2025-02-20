13d2fa42b0d0d9d001c62c90aba63d37f6bfa40f
lin979 <86038968+linwy979@users.noreply.github.com>
2025-02-20 14:11:42 +0800

    Update main.c

--- hw1/main.c
+++ hw1/main.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+void runLengthEncoding(char *str) {
+    int i, count;
+
+    for (i = 0; str[i] != '\0'; i++) {
+        // Count occurrences of current character
+        count = 1;
+        while (str[i] == str[i + 1] && count < 9) {
+            count++;
+            i++;
+        }
+
+        // Print encoded character and count
+        printf("%c%d", str[i], count);
+    }
+}
+
+int main() {
+    char input[] = "AABBBCCCCddd";
+    printf("Input: %s\n", input);
+    
+    printf("Output: ");
+    runLengthEncoding(input);
+
+    return 0;
+}
